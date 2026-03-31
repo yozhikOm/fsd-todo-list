d1044215d02234f9fa54b38b245406c2fe9e4271
Матрехина Татьяна Александровна <tatiana.matrehina@geropharm.com>
2026-03-31 17:15:22 +0300

    feat: add edit task

--- src/widgets/task-list/ui/TaskItem.tsx
+++ src/widgets/task-list/ui/TaskItem.tsx
@@ -1,9 +1,12 @@
 import type { Task } from '@/entities/task';
 import { useTaskStore } from '@/entities/task/model/store';
-import styles from './TaskItem.module.css';
 import { useState } from 'react';
 import { TaskActionsMenu } from '@/entities/task/ui/TaskActionsMenu/TaskActionsMenu';
 import { Dialog } from '@/shared/ui/dialog/Dialog';
+import { EditTask } from '@/features/edit-task';
+
+import styles from './TaskItem.module.css';
+
 
 type Props = {
   task: Task;
@@ -13,52 +16,60 @@ export const TaskItem = ({ task }: Props) => {
   const { toggleTask, deleteTask } = useTaskStore();
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
   const [isConfirmDialogOpen, setIsConfirmDialogOpen] = useState<boolean>(false);
+  const [isEditFormOpen, setIsEditFormOpen] = useState<boolean>(false);
 
   return (
-    <div className={styles.container}>
-      <div className={styles.taskCheckbox}>
-        <input
-          type='checkbox'
-          checked={task.completed}
-          onChange={() => toggleTask(task.id)}
-        />
-      </div>
-      <div className={styles.itemContent}>
-        <div className={styles.itemTitle}>
-          <div
-            style={{
-              /* TODO вынести в стили */
-              textDecoration: task.completed ? 'line-through' : 'none',
-              fontSize: 16,
-            }}
-          >
-            {task.title}
+    <>
+      <div className={styles.container}>
+        <div className={styles.taskCheckbox}>
+          <input
+            type='checkbox'
+            checked={task.completed}
+            onChange={() => toggleTask(task.id)}
+          />
+        </div>
+        <div className={styles.itemContent}>
+          <div className={styles.itemTitle}>
+            <div
+              style={{
+                /* TODO вынести в стили */
+                textDecoration: task.completed ? 'line-through' : 'none',
+                fontSize: 16,
+              }}
+            >
+              {task.title}
+            </div>
+          </div>
+          <div className={styles.itemDate}>
+            {/* TODO дата пока мок */}
+            Сегодня
           </div>
         </div>
-        <div className={styles.itemDate}>
-          {/* TODO дата пока мок */}
-          Сегодня
+        <div className={styles.menu}>
+          <button
+            className={styles.menuButton}
+            onClick={() => setIsMenuOpen((prev) => !prev)}
+          >
+            ...
+          </button>
+          {isMenuOpen && (
+            <TaskActionsMenu
+              onEdit={() => {
+                setIsMenuOpen(false);
+                setIsEditFormOpen(true);
+              }}
+              onDelete={() => {
+                setIsMenuOpen(false);
+                setIsConfirmDialogOpen(true);
+              }}
+              onClose={() => setIsMenuOpen(false)}
+            />
+          )}
         </div>
       </div>
-      <div className={styles.menu}>
-        <button
-          className={styles.menuButton}
-          onClick={() => setIsMenuOpen((prev) => !prev)}
-        >
-          ...
-        </button>
-        {isMenuOpen && (
-          <TaskActionsMenu
-            onEdit={function (): void {
-              throw new Error('Function not implemented.');
-            }}
-            onDelete={() => {
-              setIsConfirmDialogOpen(true);
-            }}
-            onClose={() => setIsMenuOpen(false)}
-          />
-        )}
-      </div>
+      {isEditFormOpen && (
+        <EditTask task={task} onClose={() => setIsEditFormOpen(false)} />
+      )}
       {isConfirmDialogOpen && (
         <Dialog
           open={isConfirmDialogOpen}
@@ -73,6 +84,6 @@ export const TaskItem = ({ task }: Props) => {
           }}
         />
       )}
-    </div>
+    </>
   );
 };
